cfdb41ed5a1bc95ac570621cc61832647f090b1a
Patricia Reinoso <patricia.reinoso12@gmail.com>
2015-01-18 18:41:09 -0430

    Separar funcion y agregar get()

--- reservation/rate.py
+++ reservation/rate.py
@@ -14,26 +14,32 @@ class rate():
         self._day=day
         self._night=night
     
-    def rateCompute(self,dateI,dateO):
-        assert(dateI<dateO)
-        assert(dateO>=dateI+timedelta(minutes=15))
-        assert(dateO<=dateI+timedelta(days=3))
-        totalRate=0
-        tempA=dateI
-        while tempA<dateO:
-            tempB=tempA+timedelta(hours=1)
-            if (tempA.hour<6 and tempB.hour>=6) or (tempA.hour<18 and tempB.hour>=18):
-                totalRate+=max(self._day,self._night)
+    def GetDay(self):
+        return self._day
+    
+    def GetNight(self):
+        return self._night
+    
+def rateCompute(dateI,dateO, r):
+    assert(dateI<dateO)
+    assert(dateO>=dateI+timedelta(minutes=15))
+    assert(dateO<=dateI+timedelta(days=3))
+    totalRate=0
+    tempA=dateI
+    while tempA<dateO:
+        tempB=tempA+timedelta(hours=1)
+        if (tempA.hour<6 and tempB.hour>=6) or (tempA.hour<18 and tempB.hour>=18):
+            totalRate+=max(r.GetDay(),r.GetNight())
+        else:
+            if tempA.hour<6 or tempA.hour>=18:
+                totalRate+=r.GetNight()
             else:
-                if tempA.hour<6 or tempA.hour>=18:
-                    totalRate+=self._night
-                else:
-                    totalRate+=self._day
-            tempA=tempA+timedelta(hours=1)
-        return totalRate
+                totalRate+=r.GetDay()
+        tempA=tempA+timedelta(hours=1)
+    return totalRate
 
 dateA=datetime(2015,7,5,5,56,0,0)
 dateB=datetime(2015,7,5,7,57,0,0)
 
 r=rate(Decimal(5),Decimal(7))
-print(r.rateCompute(dateA,dateB))
+print(rateCompute(dateA,dateB,r))
